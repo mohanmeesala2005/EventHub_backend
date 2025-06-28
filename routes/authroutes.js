2b2e6f9a9a5aebc2612377b300a5e5675ef4e5b5
mohanmeesala2005 <mohanmeesala2005@gmail.com>
2025-06-28 22:01:45 +0530

    final

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post('/signup', registerUser);
 router.post('/login', loginUser);
-router.post('/upate',updateProfile);
+router.post('/update',updateProfile);
 
 export default router;
